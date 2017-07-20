e70d04048c66cf6d4c59fd397c7f5d9fe4ec3144
Zakeria Ali <zak.fined@gmail.com>
2017-07-20 19:30:42 +0100

    Add styling for options and page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+var endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+fetch(endpoint)
+  .then(function(blob){
+    return blob.json();
+  })
+  .then(function(data){
+    console.log(data);
+  });
